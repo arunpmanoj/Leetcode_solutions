412f00bbe17ec64365219333cf8992090425de03
arunpmanoj <arunpmanoj12@gmail.com>
2025-09-07 16:34:19 +0530

    Time: 0 ms (100%), Space: 17.9 MB (56.31%) - LeetHub

--- 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.py
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.py
@@ -1,16 +1,22 @@
 class Solution:
     def sumZero(self, n: int) -> List[int]:
-        res=[]
-        i=1
-        if n%2==0:
-            while i<=n//2:
-                res.append(i)
-                res.append(-i)
-                i+=1
-        else:
-            while i<=n//2:
-                res.append(i)
-                res.append(-i)
-                i+=1
-            res.append(0)  
+        res = []
+        for i in range(1, n // 2 + 1):
+            res.extend([i, -i])
+        if n % 2 == 1:
+            res.append(0)
         return res
+        # res=[]
+        # i=1
+        # if n%2==0:
+        #     while i<=n//2:
+        #         res.append(i)
+        #         res.append(-i)
+        #         i+=1
+        # else:
+        #     while i<=n//2:
+        #         res.append(i)
+        #         res.append(-i)
+        #         i+=1
+        #     res.append(0)  
+        # return res
